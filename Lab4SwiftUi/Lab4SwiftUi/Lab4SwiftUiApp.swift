0596d305035842e935e3b89edc4353b882a01187
JuanJose <jhonajose@hotmail.es>
2024-06-03 20:18:41 +0200

    The necessary project files have begun to be created.

---
+++ Lab4SwiftUi/Lab4SwiftUi/Lab4SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab4SwiftUiApp.swift
+//  Lab4SwiftUi
+//
+//  Created by Juan jose Morales on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab4SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab4SwiftUi/Lab4SwiftUi/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Lab4SwiftUi
+//
+//  Created by Juan jose Morales on 3/6/24.
+//
+
+import Foundation
